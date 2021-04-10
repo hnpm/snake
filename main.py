6ef684defb35c53248b88fbaa69c72a6644f1472
Hien Pham <phaminhien@hotmail.com>
2021-04-10 22:44:21 +0700

    detect collision with wall

--- main.py
+++ main.py
@@ -30,4 +30,8 @@ while game_is_on:
         food.refresh()
         scoreboard.update()
 
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
 screen.exitonclick()
